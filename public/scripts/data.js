323e2cde2cbe7d7938f47c2d928c9e00515ddb84
Bosco Correa <bosco98123@gmail.com>
2019-08-17 18:01:33 +0530

    Add files via upload

---
+++ public/scripts/data.js
@@ -0,0 +1,32 @@
+var musicData = [
+  // {
+  //   name: "Voices in my head",
+  //   band: "MJ life",
+  //   image: "public/images/soundtrack_1.jpg",
+  //   file: "public/audio/audio_1.mp3"
+  // },
+  // {
+  //   name: "Spring flowers",
+  //   band: "Kashya",
+  //   image: "public/images/soundtrack_2.jpg",
+  //   file: "public/audio/audio_2.mp3"
+  // },
+  {
+    name: "Mix band",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "https://github.com/Bosco98/test/blob/master/Track%201.ogg?raw=true"
+  },
+  {
+    name: "band",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "https://github.com/Bosco98/test/blob/master/Track%202.ogg?raw=true"
+  },
+  {
+    name: "band",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "https://github.com/Bosco98/test/blob/master/Track%205.ogg?raw=true"
+  },
+];
