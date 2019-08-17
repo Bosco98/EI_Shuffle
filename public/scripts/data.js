c2472604bbaab83f33227e19e2aafaf02a83eddd
Bosco Correa <bosco98123@gmail.com>
2019-08-17 20:21:02 +0530

    Major update

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -1,32 +1,182 @@
 var musicData = [
-  // {
-  //   name: "Voices in my head",
-  //   band: "MJ life",
-  //   image: "public/images/soundtrack_1.jpg",
-  //   file: "public/audio/audio_1.mp3"
-  // },
-  // {
-  //   name: "Spring flowers",
-  //   band: "Kashya",
-  //   image: "public/images/soundtrack_2.jpg",
-  //   file: "public/audio/audio_2.mp3"
-  // },
   {
-    name: "Mix band",
-    band: "Unknown",
+    name: "Varadala Yaa ",
+    band: "Nelson & Marita",
     image: "public/images/def.png",
-    file: "https://github.com/Bosco98/test/blob/master/Track%201.ogg?raw=true"
+    file: "public/audio/track (1).ogg"
   },
   {
-    name: "band",
-    band: "Unknown",
+    name: "dhakterancha dhakter",
+    band: "victor & lucy",
     image: "public/images/def.png",
-    file: "https://github.com/Bosco98/test/blob/master/Track%202.ogg?raw=true"
+    file: "public/audio/track (2).ogg"
   },
   {
-    name: "band",
+    name: "maiechi annie",
+    band: "valentine & noreen",
+    image: "public/images/def.png",
+    file: "public/audio/track (3).ogg"
+  },
+  {
+    name: "silver jubliee",
+    band: "hector",
+    image: "public/images/def.png",
+    file: "public/audio/track (4).ogg"
+  },
+  {
+    name: "sokracha varad",
+    band: "austin",
+    image: "public/images/def.png",
+    file: "public/audio/track (5).ogg"
+  },
+  {
+    name: "sokri",
+    band: "victor & lucy",
+    image: "public/images/def.png",
+    file: "public/audio/track (6).ogg"
+  },  
+  {
+    name: "panch",
     band: "Unknown",
     image: "public/images/def.png",
-    file: "https://github.com/Bosco98/test/blob/master/Track%205.ogg?raw=true"
+    file: "public/audio/track (7).ogg"
+  },
+  {
+    name: "Bhari Magay",
+    band: "Claudette & Johny",
+    image: "public/images/def.png",
+    file: "public/audio/track (8).ogg"
+  },
+  {
+    name: "Disco Diwani",
+    band: "Nancy",
+    image: "public/images/def.png",
+    file: "public/audio/track (9).ogg"
+  },
+  {
+    name: "Ghabartas Kala",
+    band: "Austin & Merry",
+    image: "public/images/def.png",
+    file: "public/audio/track (10).ogg"
+  },
+  {
+    name: "Kolyachi Pori",
+    band: "Stany & Lucy",
+    image: "public/images/def.png",
+    file: "public/audio/track (11).ogg"
+  },
+  {
+    name: "Kulbi Shetkari",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (12).ogg"
+  },
+  {
+    name: "Maan",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (13).ogg"
+  },
+  {
+    name: "Madathicha Hath",
+    band: "victor & lucy",
+    image: "public/images/def.png",
+    file: "public/audio/track (14).ogg"
+  },
+  {
+    name: "Naal",
+    band: "victor ",
+    image: "public/images/def.png",
+    file: "public/audio/track (15).ogg"
+  },
+  {
+    name: "Nasha",
+    band: "hector",
+    image: "public/images/def.png",
+    file: "public/audio/track (16).ogg"
+  },
+  {
+    name: "San",
+    band: "martha & peter",
+    image: "public/images/def.png",
+    file: "public/audio/track (17).ogg"
+  },
+  {
+    name: "Sausar",
+    band: "victor",
+    image: "public/images/def.png",
+    file: "public/audio/track (18).ogg"
+  },
+  {
+    name: "SUKI RE MYRA",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (19).ogg"
+  },
+  {
+    name: "Unknown",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (20).ogg"
+  },
+  {
+    name: "Upkar",
+    band: "gilbert",
+    image: "public/images/def.png",
+    file: "public/audio/track (21).ogg"
+  },
+  {
+    name: "Varad",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (22).ogg"
+  },
+  {
+    name: "avalad",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (23).ogg"
+  },
+  {
+    name: "bajia vastai bara",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (24).ogg"
+  },
+  {
+    name: "comjow",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (25).ogg"
+  },
+  {
+    name: "deool",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (26).ogg"
+  },
+  {
+    name: "feeladcha varad ",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (27).ogg"
+  },
+  {
+    name: "pawool",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (28).ogg"
+  },
+  {
+    name: "Andhala Sokara",
+    band: "victor",
+    image: "public/images/def.png",
+    file: "public/audio/track (29).ogg"
+  },
+  {
+    name: "Bhow Maza Nengalai Navra",
+    band: "Unknown",
+    image: "public/images/def.png",
+    file: "public/audio/track (30).ogg"
   },
 ];
